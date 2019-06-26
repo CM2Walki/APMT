2504d7054ee7378baf142d72e6bee6b49471cd2c
Walentin <Walentin.Lamonos@lrz.de>
2019-06-26 12:50:08 +0200

    More changes

--- server/app/index.js
+++ server/app/index.js
@@ -15,12 +15,10 @@ const awsAutoscaleKubernetesRoutes  = require('./routes/awskubernetes');
 
 // Passport session setup.
 passport.serializeUser(function(user, done) {
-  //console.log("serializing " + user.username);
   done(null, user);
 });
 
 passport.deserializeUser(function(obj, done) {
-  //console.log("deserializing " + obj);
   done(null, obj);
 });
 
